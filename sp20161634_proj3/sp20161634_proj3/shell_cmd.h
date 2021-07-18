a8df09c7c4acb41fd0e62c858e64a8a4d7241f19
catensia <53655119+catensia@users.noreply.github.com>
2021-07-18 12:16:54 +0900

    Delete sp20161634_proj3/sp20161634_proj3 directory

--- sp20161634_proj3/sp20161634_proj3/shell_cmd.h
+++
@@ -1,9 +0,0 @@
-#include "defines.h"
-
-void symtab_print(struct NODE **head);
-void history_add(char command[], struct NODE **head);
-void history_print(struct NODE **head);
-void shell_dir(void);
-void shell_help(void);
-void type_filename(const char *filename, struct NODE **head);
-char* trim(char *cmd);
